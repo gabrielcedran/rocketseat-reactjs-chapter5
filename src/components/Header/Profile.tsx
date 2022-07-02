c1f6ecc5c05687d7e56afa03034346318cdc0c0c
gabrielcedran <gabrielcedran@gmail.com>
2022-07-02 14:18:22 +0100

    Tweak header responsivEness

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,27 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileDetails?: boolean
+}
+
+export function Profile({showProfileDetails = true}: ProfileProps) {
     return (
         
     <Flex
         align="center"
     >
-        <Box mr="4" textAlign="right">
-            <Text>Don Bob</Text>
-            <Text
-                color="gray.300"
-                fontSize="small"
-            >
-                donbob@bobdon.com
-            </Text>
-        </Box>
+        {
+            showProfileDetails &&
+            <Box mr="4" textAlign="right">
+                <Text>Don Bob</Text>
+                <Text
+                    color="gray.300"
+                    fontSize="small"
+                >
+                    donbob@bobdon.com
+                </Text>
+            </Box>
+        }
 
         <Avatar size="md" name="Don Bob" src="https://github.com/gabrielcedran.png" />
     </Flex>
