91ab4d66a5f3ace78ec6671e50ac1f07e3caddb2
gabrielcedran <gabrielcedran@gmail.com>
2022-07-02 17:26:59 +0100

    Add react hook form and add basic functionality on login form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,11 +1,17 @@
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
     name: string;
     label?: string;
+    // ref?: element; would never work because refs are an special component in react and needs special handling (known as ref forwarding)
+    // 1st convert regular function into arrow function if necessary - forwardRef only works with arrow function.
+    // 2nd wrap the component with react's forwardRef function - it will add the ref property.
+    // 3rd receive ref as the second parameter of the function (after props)
+    // 4th to type the ref parameter (and avoid any) and use the native ForwardRefRenderFunction component (first generics is the ref type e.g HTMLInputElement, second the props type)
 } 
 
-export function Input({name, label, ...rest}: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({name, label, ...rest}, ref) => {
     return(
         <FormControl>
             <FormLabel htmlFor={name}>{label}</FormLabel>
@@ -20,8 +26,11 @@ export function Input({name, label, ...rest}: InputProps) {
                     bgCOlor: "gray.900"
                 }}
                 size="lg"
+                ref={ref}
                 { ...rest }
             />
         </FormControl>
     )
 }
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+
+  // formState has many properties to help form handling
+  const {register, handleSubmit, formState } = useForm()
+
+
+  // to avoid 'any' in the handle fuction parameter, the type can be declared directly in the function definition 
+  // or in the parameters themselves. 
+  // The first is preferable as it also covers the second parameter type (event) in case it needs to be used.
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve=> setTimeout(resolve, 2000));
+    
+    console.log(values);
+  }
+
   return (
     <Flex 
       w="100vw" 
@@ -18,21 +39,27 @@ export default function SignIn() {
         borderRadius={8}
         flexDir="column"
         spacing={4}
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Input             
           name="email" 
           type="email" 
-          label="Email"/>
+          label="Email"
+          {...register("email")}
+        />
 
         <Input             
           name="password" 
           type="password" 
-          label="Password"/>
+          label="Password"
+          {...register("password")}
+        />
           
         <Button 
           type="submit" 
           colorScheme="pink"
           size="lg"
+          isLoading={formState.isSubmitting}
         >
           Entrar
         </Button>
